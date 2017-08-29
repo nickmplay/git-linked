b7329fe3feb23f2ed2bf2badc5198e9cc48d48d0
nm <nickmontagni@hotmail.com>
2017-08-29 20:54:03 +0100

    basic server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+var app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+app.get('/', (req, res) => {
+	res.send('Hi there from your app');
+});
+
+
+console.log("hi there");
